d557c255cdbc7b5547c97dd38fe778f5ced99e2c
arrrrrmin <info@dotarmin.info>
2022-09-27 16:47:21 +0200

    📚: Arxiv urls in package string

---
+++ esc_learner/wavemxnet/__init__.py
@@ -0,0 +1,4 @@
+""" PyTorch implementation of "Very Deep Convolutional Neural Networks for Raw Waveforms"
+    By Boqing Zhu, Changjian Wang, Feng Liu, Jin Lei, Zengquan Lu and Yuxing Peng (2016)
+    https://arxiv.org/abs/1610.00087
+"""
